04520e18a422101bc665c08f59e340471a4eafc0
loong <zloongjiang@tencent.com>
2016-12-09 13:14:48 +0800

    添加热更新和多文件编译输出

--- dev-server.js
+++ dev-server.js
@@ -5,25 +5,44 @@ var port = process.env.port || 3000;
 /*
 *引入webpack及其配置config
 */
-var webpack=require("webpack");
-var webpackConfig=require("./webpack.config");
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config");
+
+//var devClient = 'webpack-hot-middleware/client?noInfo=true&reload=true';//把这个放到dev-client.js里面，以便热更新html
+var devClient = './dev-client';
+Object.keys(webpackConfig.entry).forEach(function (name, i) {
+    var extras = [devClient];
+    webpackConfig.entry[name] = extras.concat(webpackConfig.entry[name]);
+});
+console.log(webpackConfig.entry);
+
 //调用配置,生成 compiler instance(编译实例)
 var compiler = webpack(webpackConfig);
 
-//使用webpack-dev-middleware插件
-var devMiddleware=require("webpack-dev-middleware")(compiler,{
-    publicPath:webpackConfig.output.publicPath,
-    stats:{
-        colors:true,
-        chunks:false
+//使用webpack-dev-middleware插件《自动编译》
+var devMiddleware = require("webpack-dev-middleware")(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+        colors: true,
+        chunks: false
     }
 });
 
 //注册到express服务
 app.use(devMiddleware);
-//引入热更新插件
-var hotMiddleware=require("webpack-hot-middleware")(compiler);
+//引入热更新插件《自动刷新》
+var hotMiddleware = require("webpack-hot-middleware")(compiler);
 app.use(hotMiddleware);
+
+
+//监听html文件改变事件
+compiler.plugin("compilation", function (compilation) {
+    compilation.plugin("html-webpack-plugin-after-emit", function (data, cb) {
+        //发布reload事件,在dev-client.js监听
+        hotMiddleware.publish({ action: "reload" });
+        cb();
+    })
+});
 
 //使用静态资源
 app.use(express.static(__dirname + "/"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
 var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
-var webpack=require("webpack");
+var webpack = require("webpack");
+var fs=require("fs");
+
+//获取app下面的所有输出js,多页面嘛
+function getEntry() {
+    //return path.resolve(__dirname, "./app/main.js");
+    var jsPath = fs.readdirSync("./app/");   
+    var matchs = [], files = {};
+    jsPath.forEach(function (item) {
+       matchs=item.match(/(.+)\.js$/); //查找app下的js文件     
+       if(matchs){
+           files[matchs[1]]=path.resolve(__dirname+"/app/",item);        
+       }
+    });
+    return files;
+}
+
+function pages() {
+    var htmlPath=fs.readdirSync(path.resolve(__dirname,"./app/views/"));
+    var matchs = [], files = {};
+    htmlPath.forEach(function (item) {
+        matchs=item.match(/(.+)\.html$/);
+        console.log(matchs);
+    });
+}
+pages();
 module.exports = {
+    /*
     entry: {
-        //app: path.resolve(__dirname, "./app/main.js")
-        app:["webpack-hot-middleware/client",path.resolve(__dirname,"./app/main.js")]
+        app: path.resolve(__dirname, "./app/main.js") //可以把下面webpack-hot-xx的相关信息放到dev-server.js中去动态添加
+        //webpack-hot-middleware/client开启热加载，noInfo=true不要console显示编译信息
+        //app:["webpack-hot-middleware/client?noInfo=true&reload=true",path.resolve(__dirname,"./app/main.js")]
     },
+    */
+    entry:getEntry(), //这样可以方便支持多文件
     output: {
         path: path.resolve(__dirname, "./output/static"),
         publicPath: "/",//调试或者 CDN 之类的域名
@@ -26,11 +55,20 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({      
-            filename: 'index.html',//渲染输出html文件名,路径相对于 output.path 的值
-            template: path.resolve(__dirname, './app/views/index.html'), //渲染源模版文件
-            inject: true //这个东西非常重要，true: 自动写入依赖文件; false: 不写入依赖，构建多页面非常有用
-        }),
+        //这里的HtmlWebpackPlugin可以做动态添加
+        // new HtmlWebpackPlugin({
+        //     filename: 'index.html',//渲染输出html文件名,路径相对于 output.path 的值
+        //     template: path.resolve(__dirname, './app/views/index.html'), //渲染源模版文件
+        //     inject: false //这个东西非常重要，true: 自动写入依赖文件; false: 不写入依赖，构建多页面非常有用
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'home.html',//渲染输出html文件名,路径相对于 output.path 的值
+        //     template: path.resolve(__dirname, './app/views/home.html'), //渲染源模版文件
+        //     inject: false //这个东西非常重要，true: 自动写入依赖文件; false: 不写入依赖，构建多页面非常有用
+        // }),
+
+
+
         new webpack.HotModuleReplacementPlugin()
     ]
 };
